337d4df4399da6c12e5c4882845fa17322ec0a1d
Victor Wibisono <devacto@gmail.com>
2018-11-09 15:29:35 +0800

    Bare bones Spark app.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,7 @@ application {
 
 dependencies {
     compile(kotlin("stdlib"))
-    compile(group = "org.apache.spark", name = "spark-core_2.11", version = "2.4.0")
     compile(group = "org.apache.spark", name = "spark-sql_2.11", version = "2.4.0")
-    compile(group = "org.slf4j", name = "slf4j-log4j12", version = "1.7.25")
 }
 
 repositories {
--- src/main/kotlin/sample/Processor.kt
+++ src/main/kotlin/sample/Processor.kt
@@ -2,7 +2,20 @@ package sample
 
 import org.apache.spark.sql.SparkSession
 
-
 fun main(args: Array<String>) {
-    val sparkSession = SparkSession.builder().master("local")
+    val sparkSession = SparkSession.builder()
+            .master("local")
+            .appName("rr-processor")
+            .config("spark.driver.extraJavaOptions", "-Djava.net.userSystemProxies=true")
+            .config("spark.sql.parquet.binaryAsString", "true")
+            .orCreate
+
+    val sparkContext = sparkSession.sparkContext()
+
+    println("Spark version: ${sparkContext.version()}")
+    println("Spark master:  ${sparkContext.master()}")
+    println("Spark running locally: ${sparkContext.isLocal}")
+    println("Default parallelism: ${sparkContext.defaultParallelism()}")
+
+    sparkSession.close()
 }
